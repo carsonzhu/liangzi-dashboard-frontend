b11ed41fffa44e865c666861e0974fc8e55712ef
Edward Feng <edward0414@gmail.com>
2019-06-28 17:46:57 -0400

    use signinform

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Home.css";
 
-export default () => {
-  return <div className="home-route">HOME</div>;
+import SignInForm from "../../components/Forms/SignInForm";
+
+const Home = () => {
+  return (
+    <div className="home-route">
+      <div>HOME</div>
+      <SignInForm />
+    </div>
+  );
 };
+
+export default Home;

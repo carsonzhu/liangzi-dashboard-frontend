30cb963aae85be2e1037c311b5160606d600a2a9
Edward Feng <edward0414@gmail.com>
2019-06-28 17:11:17 -0400

    add config file

---
+++ src/routes/Root/config.js
@@ -0,0 +1,14 @@
+// Fake data
+const urlTabs = [
+  {
+    title: "量子租车(Logo)",
+    link: "#"
+  },
+  { title: "国际租车", link: "#" },
+  { title: "驾照翻译", link: "#" },
+  { title: "保险服务", link: "#" },
+  { title: "特价优惠", link: "#" },
+  { title: "注册/登录", link: "#" }
+];
+
+export { urlTabs };

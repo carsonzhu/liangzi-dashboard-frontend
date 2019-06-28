6f9c67f3dc86d17c4d3d3ff8a99894f48780d417
Edward Feng <edward0414@gmail.com>
2019-06-28 17:18:34 -0400

    add component

--- src/routes/Root/config.js
+++ src/routes/Root/config.js
@@ -1,14 +1,17 @@
+// TODO: Import component!
+
 // Fake data
 const urlTabs = [
   {
     title: "量子租车(Logo)",
-    link: "#"
+    link: "#",
+    component: () => {}
   },
-  { title: "国际租车", link: "#" },
-  { title: "驾照翻译", link: "#" },
-  { title: "保险服务", link: "#" },
-  { title: "特价优惠", link: "#" },
-  { title: "注册/登录", link: "#" }
+  { title: "国际租车", link: "#", component: () => {} },
+  { title: "驾照翻译", link: "#", component: () => {} },
+  { title: "保险服务", link: "#", component: () => {} },
+  { title: "特价优惠", link: "#", component: () => {} },
+  { title: "注册/登录", link: "#", component: () => {} }
 ];
 
 export { urlTabs };

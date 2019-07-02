4a71218546117b43825875ca249136d8aa835166
Edward Feng <edward0414@gmail.com>
2019-07-02 10:31:27 -0400

    transaction init

---
+++ src/routes/Transactions/Transactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Transactions.css";
+
+const Transactions = () => {
+  return (
+    <div className="transactions-route">
+      <div>Transactions</div>
+    </div>
+  );
+};
+
+export default Transactions;

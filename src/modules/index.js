cf3880a447d30e7ae41ce1e5a490a86a89c1f0e7
Edward Feng <edward0414@gmail.com>
2019-07-02 11:33:08 -0400

    add redux stre

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import { all } from "redux-saga/effects";
+import appReducer from "../reducers";
 
 export const createRootReducer = history =>
   combineReducers({
+    ...appReducer,
     router: connectRouter(history)
   });
 

00769dc83532851c8863e7603ce8a7a4a32d6fce
Edward Feng <edward0414@gmail.com>
2019-07-02 11:05:50 -0400

    add index

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+import cars from "./cars";
+import login from "./login";
+import tickets from "./tickets";
+import transactions from "./transactions";
+import users from "./users";
+
+const appReducer = {
+  cars,
+  login,
+  tickets,
+  transactions,
+  users
+};
+
+export default appReducer;

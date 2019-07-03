4a3010b4b80b83d9adb5d0d2ab99375305ebadfe
Edward Feng <edward0414@gmail.com>
2019-07-03 18:36:39 -0400

    remove actions

--- src/actioins/componentState.js
+++
@@ -1,9 +0,0 @@
-const openLoginModal = () => {
-  return { type: "OPEN_LOGIN_MODAL" };
-};
-
-const closeLoginModal = () => {
-  return { type: "CLOSE_LOGIN_MODAL" };
-};
-
-export { openLoginModal, closeLoginModal };

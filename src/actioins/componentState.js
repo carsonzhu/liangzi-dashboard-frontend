070ce7043718d29158522387306dcc0903457935
Edward Feng <edward0414@gmail.com>
2019-07-03 16:03:51 -0400

    add actioners

---
+++ src/actioins/componentState.js
@@ -0,0 +1,9 @@
+const openLoginModal = () => {
+  return { type: "OPEN_LOGIN_MODAL" };
+};
+
+const closeLoginModal = () => {
+  return { type: "CLOSE_LOGIN_MODAL" };
+};
+
+export { openLoginModal, closeLoginModal };

ec433049589b97aff710f54aaf60ca5cb5e59a2d
Edward Feng <edward0414@gmail.com>
2019-07-05 09:32:57 -0400

    fake data

--- src/routes/Admins/Admins.jsx
+++ src/routes/Admins/Admins.jsx
@@ -1,10 +1,71 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 import "./Admins.css";
+
+//Fake data
+const data = [
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  },
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  },
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  },
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  },
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  },
+  {
+    email: "abc@abc.com",
+    company: "Alamo",
+    allowedOperations: ["cars", "users", "insurances", "transactions"]
+  }
+];
+
+const operationGenerator = operations => {
+  return operations.map(operation => <ul>{operation}</ul>);
+};
+
+const tbodyGenerator = admins => {
+  return admins.map((info, ind) => (
+    <tr key={ind}>
+      <td>{ind}</td>
+      <td>{info.email}</td>
+      <td>{info.company}</td>
+      <td>{operationGenerator(info.allowedOperations)}</td>
+    </tr>
+  ));
+};
 
 const Admins = () => {
   return (
     <div className="admins-route">
       <div>Admins</div>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Allowed Operations</th>
+          </tr>
+        </thead>
+        <tbody>{tbodyGenerator(data)}</tbody>
+      </Table>
     </div>
   );
 };
--- src/routes/Cars/Cars.jsx
+++ src/routes/Cars/Cars.jsx
@@ -1,10 +1,85 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 import "./Cars.css";
+
+//Fake data
+const data = [
+  {
+    dailyRate: 100,
+    pickupLocation: "Toronto",
+    returnLocation: "Toronto",
+    specialServices: ["GPS", "驾照翻译件", "儿童安全座椅", "中文向导"],
+    transmission: "AUTOMATIC",
+    vehicleTypeId: "Toyota",
+    rentalCompany: "Aloma"
+  },
+  {
+    dailyRate: 100,
+    pickupLocation: "Toronto",
+    returnLocation: "Toronto",
+    specialServices: ["GPS", "驾照翻译件", "儿童安全座椅", "中文向导"],
+    transmission: "AUTOMATIC",
+    vehicleTypeId: "Toyota",
+    rentalCompany: "Aloma"
+  },
+  {
+    dailyRate: 100,
+    pickupLocation: "Toronto",
+    returnLocation: "Toronto",
+    specialServices: ["GPS", "驾照翻译件", "儿童安全座椅", "中文向导"],
+    transmission: "AUTOMATIC",
+    vehicleTypeId: "Toyota",
+    rentalCompany: "Aloma"
+  },
+  {
+    dailyRate: 100,
+    pickupLocation: "Toronto",
+    returnLocation: "Toronto",
+    specialServices: ["GPS", "驾照翻译件", "儿童安全座椅", "中文向导"],
+    transmission: "AUTOMATIC",
+    vehicleTypeId: "Toyota",
+    rentalCompany: "Aloma"
+  }
+];
+
+const operationGenerator = operations => {
+  return operations.map((operation, ind) => <ul key={ind}>{operation}</ul>);
+};
+
+const tbodyGenerator = Cars => {
+  return Cars.map((info, ind) => (
+    <tr key={ind}>
+      <td>{ind}</td>
+      <td>{info.dailyRate}</td>
+      <td>{info.pickupLocation}</td>
+      <td>{info.returnLocation}</td>
+      <td>{operationGenerator(info.specialServices)}</td>
+      <td>{info.transmission}</td>
+      <td>{info.vehicleTypeId}</td>
+      <td>{info.rentalCompany}</td>
+    </tr>
+  ));
+};
 
 const Cars = () => {
   return (
     <div className="cars-route">
       <div>Cars</div>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Daily Rate</th>
+            <th>Pickup Location</th>
+            <th>Return Location</th>
+            <th>Special Services</th>
+            <th>Transmission</th>
+            <th>Vehicle Type</th>
+            <th>Rental Company</th>
+          </tr>
+        </thead>
+        <tbody>{tbodyGenerator(data)}</tbody>
+      </Table>
     </div>
   );
 };

d344e8112ee3b34bba78e3e1cdc315c9bd2e6348
Edward Feng <edward0414@gmail.com>
2019-07-12 10:46:33 -0400

    wip..

--- src/components/Modal/adminModal/adminModal.jsx
+++ src/components/Modal/adminModal/adminModal.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Formik } from "formik";
-import { Alert, Form, Modal, Button } from "react-bootstrap";
+import { Alert, Form, Modal, Button, Col } from "react-bootstrap";
 import "./adminModal.css";
+
+import {
+  inputGroup,
+  radioButtonGroup,
+  optionGroup
+} from "../../Forms/FormGroup";
 
 class AdminModal extends Component {
   static propTypes = {
@@ -24,7 +30,7 @@ class AdminModal extends Component {
     }));
   }
 
-  formGenerator({ props, data, beingEdited }) {
+  formGenerator({ props, beingEdited }) {
     const inputTypeHelper = key => {
       switch (key) {
         case "email":
@@ -39,16 +45,22 @@ class AdminModal extends Component {
     const labelHelper = key => {
       switch (key) {
         case "_id":
-          return "UserId (not editabled)";
+          return "UserId";
         case "userType":
           return "User Type";
         case "allowedOperations":
           return "Allowed Operations";
         case "password":
-          return "Password (not editabled)";
+          return "Password";
+        case "isActive":
+          return "Active Account";
         default:
           return key;
       }
+    };
+
+    const disabledLogic = key => {
+      return !(beingEdited && notEditabled.indexOf(key) === -1);
     };
 
     const inputOrders = [
@@ -57,23 +69,70 @@ class AdminModal extends Component {
       "email",
       "password",
       "userType",
-      "allowedOperations"
+      "allowedOperations",
+      "isActive"
     ];
     const notEditabled = ["password", "_id"];
 
-    return inputOrders.map(key => (
-      <Form.Group controlId={`form-${key}`}>
-        <Form.Label className="modal__capitalized">
-          {labelHelper(key)}
-        </Form.Label>
-        <Form.Control
-          type={inputTypeHelper(key)}
-          value={key !== "password" ? props.values[key] : "12345678"}
-          name={key}
-          disabled={!beingEdited || notEditabled.indexOf(key) === -1}
-        />
-      </Form.Group>
-    ));
+    return inputOrders.map(key => {
+      if (key === "allowedOperations") {
+        return radioButtonGroup({
+          label: labelHelper(key),
+          name: key,
+          disabled: disabledLogic(key),
+          labelClass: "modal__capitalized",
+          checkGroupClass: "modal__checkGroupClass",
+          radioValues: [
+            {
+              label: "Vehicle Operations",
+              name: "cars",
+              id: "allowed-operations__cars",
+              checked: props.values[key].indexOf("cars") !== -1
+            },
+            {
+              label: "User Operations",
+              name: "users",
+              id: "allowed-operations__users",
+              checked: props.values[key].indexOf("users") !== -1
+            },
+            {
+              label: "Insurance Operations",
+              name: "insurances",
+              id: "allowed-operations__insurances",
+              checked: props.values[key].indexOf("insurances") !== -1
+            },
+            {
+              label: "Transaction Operations",
+              name: "transactions",
+              id: "allowed-operations__transactions",
+              checked: props.values[key].indexOf("transactions") !== -1
+            }
+          ]
+        });
+      } else if (key === "isActive") {
+        return optionGroup({
+          label: labelHelper(key),
+          type: inputTypeHelper(key),
+          value: key !== "password" ? props.values[key] : "12345678",
+          name: key,
+          disabled: disabledLogic(key),
+          labelClass: "modal__capitalized",
+          optionValues: [
+            { label: "Yes", value: true, selected: props.values[key] },
+            { label: "No", value: false, selected: !props.values[key] }
+          ]
+        });
+      } else {
+        return inputGroup({
+          label: labelHelper(key),
+          type: inputTypeHelper(key),
+          value: key !== "password" ? props.values[key] : "12345678",
+          name: key,
+          disabled: disabledLogic(key),
+          labelClass: "modal__capitalized"
+        });
+      }
+    });
   }
 
   createForm({ data, onSubmit, beingEdited, handleClose, handleEdit }) {
@@ -113,22 +172,25 @@ class AdminModal extends Component {
     const { toShow, handleClose, handleEdit, data } = this.props;
 
     return (
-      <div className="adminModal">
-        <Modal show={toShow} size="lg" onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>{data.username}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {this.createForm({
-              data,
-              onSubmit: handleEdit,
-              beingEdited: this.state.beingEdited,
-              handleClose,
-              handleEdit
-            })}
-          </Modal.Body>
-        </Modal>
-      </div>
+      <Modal
+        className="adminModal"
+        show={toShow}
+        size="lg"
+        onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{data.username}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {this.createForm({
+            data,
+            onSubmit: handleEdit,
+            beingEdited: this.state.beingEdited,
+            handleClose,
+            handleEdit
+          })}
+        </Modal.Body>
+      </Modal>
     );
   }
 }

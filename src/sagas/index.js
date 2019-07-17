972b5c41596858fe11ec765ccab3829b58be85f0
Edward Feng <edward0414@gmail.com>
2019-07-17 16:39:54 -0400

    add index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,7 +2,8 @@ import { all } from "redux-saga/effects";
 import Login from "./login";
 import ComponentState from "./componentState";
 import Admins from "./admins";
+import Cars from "./cars";
 
 export default function* rootSaga() {
-  yield all([...Login, ...ComponentState, ...Admins]);
+  yield all([...Login, ...ComponentState, ...Admins, ...Cars]);
 }
